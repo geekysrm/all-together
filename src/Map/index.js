8e8b225060ddcac95d1c98633fd50ded1fd7e54f
kiminohero <bishal5058prime@gmail.com>
2020-04-12 20:25:00 +0530

    adding placeServices

--- src/Map/index.js
+++ src/Map/index.js
@@ -5,9 +5,12 @@ import Loading from "../Loader";
 class Maps extends Component {
   constructor(props) {
     super(props);
+    this.placesServices = null;
+    this.autocompleteServices = null;
     this.state = {
-      latitude: 20.951665800000004,
-      longitude: 85.0985236,
+      latitude: 20.378425,
+      longitude: 85.825146,
+      markers: [],
     };
   }
 
@@ -22,26 +25,43 @@ class Maps extends Component {
         (error) => error
       );
     }
+
+    if (window.google.maps.places.PlacesServiceStatus) {
+      this.placesServices = window.google.maps.places.PlacesService;
+    }
+    if (window.google.maps.places.AutocompleteService) {
+      this.autocompleteServices = window.google.maps.places.AutocompleteService;
+    }
   }
 
   render() {
-    const { longitude, latitude } = this.state;
+    const { longitude, latitude, markers } = this.state;
 
     return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={{
-          height: "100%",
-          width: "100%",
-        }}
-        {...(window.navigator.geolocation && {
-          initialCenter: {
-            lng: longitude,
+      <>
+        <Map
+          google={this.props.google}
+          zoom={14}
+          style={{
+            height: "100%",
+            width: "100%",
+          }}
+          {...(window.navigator.geolocation && {
+            initialCenter: {
+              lng: longitude,
+              lat: latitude,
+            },
+          })}
+          center={{
             lat: latitude,
-          },
-        })}
-      />
+            lng: longitude,
+          }}
+        >
+          {markers.map((mark) => (
+            <pre>{JSON.stringify(mark, null, 2)}</pre>
+          ))}
+        </Map>
+      </>
     );
   }
 }
